39f41918588afe70ca07e09e5435ccd2c3c784bb
Saad Satter <saadsatter@gmail.com>
2021-09-05 11:56:55 -0400

    More notes

--- src/App.js
+++ src/App.js
@@ -5,6 +5,24 @@ import Tasks from './Component/Tasks';
 //ctrl ~ for terminal
 //ctrl c to stop server
 //to open application windows powershell -> cd task-tracker-react -> code .
+/*
+Notes on REACT
+  Props vs State:
+    State: this is an object/array that is immutable and can only be changed through setTask.
+            It is only visible to the component it is in. States should be used in the main file and pass through
+            to the component as prop
+    Prop: A object that can be passed through file to file as a parameter to components 
+  
+  Lifecycle Hooks: There are three phases to a life cycle of a component. 
+                    Mounting Phase which is initializing the component
+                    Updating Phase which deals with interacting with the component during its life cycle 
+                    Un-mounting Phase Which deals with how the component gets deleted 
+                  Hooks can interact with the component at any moment during its life cycle.
+                  THEY CAN ONLY BE USED IN CLASS COMPONENTS
+  
+  Arrow Function: name = (param) => {function body}
+                  This a short hand in writing functions 
+*/
 
 function App() {
   const name = "brad"
